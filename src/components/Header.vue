d6a7c23d081f5417eef3625bfafcc181e29e82ae
haegeum21 <omarkul.md@gmail.com>
2023-08-17 09:32:48 +0600

    setup

---
+++ src/components/Header.vue
@@ -0,0 +1,14 @@
+<template>
+<div>in a real Header</div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'MyHeader'
+  }
+  </script>
+  
+  <!-- Add "scoped" attribute to limit CSS to this component only -->
+  <style scoped>
+  </style>
+  
